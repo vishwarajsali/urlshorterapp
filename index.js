27bfa2bbae7f76f85dc623c0aad27c43b25fe5cd
Vishwaraj <sali.vishwaraj@gmail.com>
2020-06-25 18:09:46 -0500

    post uri

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup')
+const monk = require('monk');
+const { nanoid } = require('nanoid')
+
+require('dotenv').config();
+
+const db = monk(process.env.MONGO_URI)
+const urls = db.get('urls');
+urls.createIndex('name')
 
 const app = express();
 
@@ -11,37 +20,75 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
 
-// app.get('/', (req, res) => {
-//     //TODO: redirect to URL
-//     res.json({
-//         mess: "test"
-//     })
-// });
 
 // app.post('/url/:id', (req, res)=> {
-//     //TODO: Get the specific URL 
-    
+//     // TODO: Get the specific URL 
+
 // })
 
 // app.get('/:id', (req, res) => {
 //     //TODO: redirect to URL
 
 // });
-app.get('/url', (req, res)=> {
-    //TODO: Create a short URL 
-    res.json({
-        messagag: "HI"
-    })
+
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required(),
 })
 
-// app.post('/url', (req, res)=> {
-//     //TODO: Create a short URL 
-     
-// })
+app.post('/url', async (req, res, next) => {
+
+    // TODO : Create a Url 
+
+    let { slug, url } = req.body;
+    try {
+
+        await schema.validate({
+            slug,
+            url,
+        });
+
+        if (!slug) {
+            slug = nanoid(5);
+
+        }
+        else {
+            const existing = await urls.findOne({slug});
+            if(existing){
+                throw new Error("slug taken 🍔")
+            }
+        }
+        slug = slug.toLowerCase();
+        const newUrl = {
+            url,
+            slug,
+        }
+
+        const created = await urls.insert(newUrl);
+        res.json(created)
+
+    } catch (error) {
+       
+        next(error)
+    }
+})
+
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status)
+    } else {
+        res.status(500)
+    }
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+    })
+})
 
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
-    
+
 })
